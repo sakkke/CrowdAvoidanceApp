c523f0dac9ac679f2824d3e3c850ffd283607b5a
Konosuke Sakai <23b3047@sit.shonan-it.ac.jp>
2024-07-09 13:41:35 +0900

    Initial Commit

---
+++ CrowdAvoidanceApp/CrowdAvoidanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrowdAvoidanceAppApp.swift
+//  CrowdAvoidanceApp
+//
+//  Created by A202 on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct CrowdAvoidanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
